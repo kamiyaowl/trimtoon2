c693dd34a3a7115472a746dbf10b4dc9c723ebb2
K <fieldprogrammable@gmail.com>
2018-12-04 23:30:21 +0900

    ReInitialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,36 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, MatProgressSpinnerModule, MatProgressBarModule, MatSnackBarModule } from '@angular/material';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { FileButtonComponent } from './component/file-button/file-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DashboardComponent,
-    FileButtonComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatGridListModule,
-    MatCardModule,
-    MatMenuModule,
-    MatProgressBarModule,
-    MatSnackBarModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
